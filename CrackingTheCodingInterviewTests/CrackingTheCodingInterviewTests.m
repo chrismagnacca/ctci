67055db183fceeae951a46fa390b30600becab4a
Chris Magnacca <chrismagnacca@gmail.com>
2013-12-23 10:23:28 -0500

    adding unit test project & gtest framework

---
+++ CrackingTheCodingInterviewTests/CrackingTheCodingInterviewTests.m
@@ -0,0 +1,34 @@
+//
+//  CrackingTheCodingInterviewTests.m
+//  CrackingTheCodingInterviewTests
+//
+//  Created by Chris Magnacca on 12/23/13.
+//  Copyright (c) 2013 Chris Magnacca. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CrackingTheCodingInterviewTests : XCTestCase
+
+@end
+
+@implementation CrackingTheCodingInterviewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
